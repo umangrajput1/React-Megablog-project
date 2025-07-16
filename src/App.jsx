a0ffccea7537a01ab30325e2da51711191da2f2b
Umang Rajput <umangkumar7800@gmail.com>
2025-07-17 02:19:43 +0530

    Use dispatch(login & logout) and add loaing funtionality and component add for check application and these funtionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import './App.css'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from "./store/authSlice"
+// import Header from './components/Header/Header'
+// import Footer from './components/Footer/Footer'
+import {Header, Footer} from './components/index'
+
+
+import { Outlet } from 'react-router-dom'
+
+
 function App() {
 
-  return (
-    <>
-        <h1>A Blog app with in Appwrite</h1>
-    </>
+  const [loading, setLoading]=useState(true)
+  const dispatch=useDispatch();
+
+  useEffect( ()=>{
+    authService.getCurrentUser()
+    .then( (userData)=>{
+      if(userData){
+        dispatch( login({userData}))
+      }else{
+        dispatch(logout(null))
+      }
+    })
+    .finally( ()=> {setLoading(false)})
+  },[])
+
+  return !loading? (
+    <div className='min-h-screen flex flex-wrap 
+    content-between bg-gray-400'>  
+      <div className='w-full block'>
+       <Header/>
+       <main>
+        ToDo {/* <Outlet/> */}
+       </main>
+       <Footer/>
+      </div>
+    </div>
   )
+
+  : <div> loading </div>
+
 }
 
 export default App
