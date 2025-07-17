8d051e1e0379336d92a634a5a303c419d5502351
Umang Rajput <umangkumar7800@gmail.com>
2025-07-17 16:27:23 +0530

    build production base components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,14 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
 
